cb56085f6c5c5d1d107ee74820483113cbd145ac
Felipe Moura <felipemouradev@gmail.com>
2023-02-10 17:16:07 -0600

    correct the folder creation

--- folders_to_create.sh
+++ folders_to_create.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-read -p file_name
-while read line; do
-    mkdir "$line"
-done < $file_name
-
+read -p "What's the file name with the folders you want to create? " file_name
+if [[ -f "$file_name" ]]; then
+    while read line; do
+	mkdir "$line"
+    done < $file_name
+else
+    echo "Not a valid file"
+fi
